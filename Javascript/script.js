9b4eba05d5fecf528f8e71dcbb36b5bd45b82f12
Maruf Khan <marufatrai12@gmail.com>
2023-11-18 10:20:47 +0600

    JS-Class-10

---
+++ Javascript/script.js
@@ -0,0 +1,12 @@
+
+    //  JavaScript Area of various Shapes .
+
+
+
+    var base = parseFloat (prompt("Enter Base : "));
+    var height = parseFloat( prompt("Enter Height : "));
+
+
+    var area = (base * height) / 2 ;
+
+    document.write("Area : " + area);
